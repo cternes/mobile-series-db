bb6f5da60a1154bf676f291c84348730410bf0a9
cternes <github@slackspace.de>
2015-08-30 12:27:51 +0200

    Adapted application to make it work with v2 of api

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 //load config file first, then bootstrap angular afterwards
 $.getJSON('js/config/config.json', function (data) {
+	apiVersion = data.apiVersion;
     apiKey = data.apiKey;
     username = data.username;
     
@@ -32,40 +33,64 @@ app.config(function() {
 });
 
 app.controller('SeriesController', function($scope, $http) {
-		
+	var apiToken = '';
+	
     $scope.$watch('online', function(newStatus) {
     });
     
     //if we're online, fetch fresh data from server and store in browser storage
     if ($scope.online) {
-        //we need to use jsonp, since normal $http does not allow cross origin requests
-        $http.jsonp('http://api.trakt.tv/user/progress/watched.json/' + apiKey + '/' + username + '?callback=JSON_CALLBACK') //callback=JSON_CALLBACK is for jsonp
-                .success(function(data) {
-                    $scope.seriesList = data;
-                    angular.forEach($scope.seriesList, function(value, key) {
-                       parseSeries(value);
-                    });
-            
-                    localStorage.setItem("seriesJson", angular.toJson($scope.seriesList));
-                })
-                .error(function(data, status) {
-                    console.log('Could not fetch data from server ('+status+'). Have you set your api key in /js/config/config.js?');
-                });
+		
+		$http.get('https://api-v2launch.trakt.tv/users/' + username + '/watched/shows', { headers: {
+			'Authorization': apiToken,
+			'trakt-api-version': apiVersion,
+			'trakt-api-key': apiKey
+			}
+		})
+		.success(function(data) {
+			$scope.seriesList = data;
+			angular.forEach($scope.seriesList, function(value, key) {
+			   getSeriesProgress(value, data);
+			});
+
+			localStorage.setItem("seriesJson", angular.toJson($scope.seriesList));
+		})
+		.error(function(data, status) {
+			console.log('Could not fetch data from server ('+status+'). Have you set your api key in /js/config/config.js?');
+		});
     }
     //if we're offline, fetch data from browser storage
     else {
         $scope.seriesList = angular.fromJson(localStorage.getItem("seriesJson"));
     }
     
-    function parseSeries(series) {
-        //if fully watched, set style class
-        if(series.progress.percentage === 100) {
-            series.styleClass = 'light-red'
-        }
-        
-        //if has next episode, get season
-        if(series.next_episode !== false) {
-            series.nextSeason = series.next_episode.season;
-        }
-    }
+	function getSeriesProgress(series, seriesList) {
+		var id = series.show.ids.trakt;
+		
+		$http.get('https://api-v2launch.trakt.tv/shows/' + id + '/progress/watched', { headers: {
+			'Authorization': apiToken,
+			'trakt-api-version': apiVersion,
+			'trakt-api-key': apiKey
+			}
+		})
+		.success(function(data) {
+			var remainingEpisodesToWatch = data.aired - data.completed;
+			
+			//if fully watched, set style class
+			if(remainingEpisodesToWatch === 0) {
+				series.styleClass = 'light-red'
+			}
+			
+			//if has next episode, get season
+			if(data.next_episode !== null) {
+				series.nextSeason = data.next_episode.season;
+			}
+			
+			// store
+			localStorage.setItem("seriesJson", angular.toJson($scope.seriesList));
+		})
+		.error(function(data, status) {
+			console.log('Could not fetch data from server ('+status+'). Have you set your api key in /js/config/config.js?');
+		});
+	}
 });
